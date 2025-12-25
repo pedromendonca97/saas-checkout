26342649ecdaa99453ccc42c46c019d2adf04033
pedromendonca97 <peedror.m@gmail.com>
2025-12-25 12:43:16 -0300

    feat: project structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import express from "express"
 const app = express()
 const PORT = 3000
 
+app.use(express.json())
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${ PORT }`)
 })
